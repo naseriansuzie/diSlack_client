1b10f7e2930c2be90c48a737bc461cebfc89e662
LEEJAEHYUB <woguqdl20@gmail.com>
2020-01-14 22:10:11 +0900

    :building_construction: Build_Components
    - 로그인, 회원가입 분기처리
    - 컴포넌트 파일 생성

--- dislackcli/src/App.js
+++ dislackcli/src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Redirect, Route, Switch, Link } from "react-router-dom";
+import { Button } from "antd";
+import Signin from "./pages/sign/SignIn";
+import SignUp from "./pages/sign/SignUp";
 
-function App() {
-  return <div className="App">초기단계</div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLogin: false,
+      workSpace: null,
+      usernfo: {},
+    };
+  }
+
+  render() {
+    const { isLogin, workSpace } = this.state;
+    return isLogin && workSpace ? (
+      <div> Main.js </div>
+    ) : (
+      <div className="App">
+        최상위 컴포넌트
+        <Link to="/signin">로그인</Link>
+        <Link to="/signup">회원가입</Link>
+        <Link to="/workspace">워크스페이스</Link>
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/signin" render={() => <Signin />} />
+          <Route path="/signup" render={() => <SignUp />} />
+          <Route path="/workspace" />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ dislackcli/src/pages/sign/SignIn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Signin extends React.Component {
+  //   constructor() {
+  //     super();
+  //   }
+
+  render() {
+    return <div>로그인</div>;
+  }
+}
+
+export default Signin;
