a5adc7d7d22e7b5107b813f44d40de5f10e3b26b
LEEJAEHYUB <woguqdl20@gmail.com>
2020-01-15 15:04:28 +0900

    :goal_net: Catch_ReactRenderErr
      - 로그인에서 리액트가 렌더될 때 나는 오류를 잡았습니다.

--- dislackcli/src/pages/sign/SignIn.js
+++ dislackcli/src/pages/sign/SignIn.js
@@ -12,6 +12,7 @@ class Signin extends React.Component {
       password: "",
     };
     this.handleInputValue = this.handleInputValue.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit = e => {
@@ -23,19 +24,60 @@ class Signin extends React.Component {
     };
     console.log(userInfo);
     // 포스트 요청하고 난 뒤 리다이렉트를 워크스페이스목록으로 이동한다
-    return (
-      <div>
-        <Redirect to="/" />
-      </div>
-    );
   };
 
   handleInputValue = key => e => {
     this.setState({ [key]: e.target.value });
+    console.log(this.state);
   };
 
   render() {
-    return <div>1</div>;
+    return (
+      <Card
+        style={{
+          borderRadius: 10,
+          marginTop: 80,
+          marginLeft: 600,
+          width: 600,
+          backgroundColor: "#bdc3c7",
+        }}
+      >
+        <Form className="login_form" onSubmit={this.handleSubmit} />
+        <Form.Item>
+          <Input
+            className="login_input"
+            style={{ height: 50 }}
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+            placeholder="Username"
+            onChange={this.handleInputValue("email")}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Input
+            style={{ height: 50 }}
+            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+            type="password"
+            placeholder="Password"
+            onChange={this.handleInputValue("password")}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            style={{
+              float: "right",
+              height: 60,
+              width: 90,
+              fontSize: 20,
+            }}
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Card>
+    );
   }
 }
 
