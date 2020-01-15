6876db11d01eaf9310848376b16c6f3bd73ac420
LEEJAEHYUB <woguqdl20@gmail.com>
2020-01-15 23:32:55 +0900

    :alien: API_Login API
    -로그인 API를 추가하였는데 아직 정확히 기능 구현은 불확실

--- dislackcli/src/App.js
+++ dislackcli/src/App.js
@@ -13,6 +13,22 @@ class App extends React.Component {
       workSpace: null,
       userInfo: {},
     };
+    this.userLogin = this.userLogin.bind(this);
+  }
+
+  userLogin() {
+    console.log("로그인되었습니다.");
+    this.setState({ isLogin: true });
+    console.log(this.state);
+    // 서버에 요청을 해서 userinfo에 데이터전달
+    // axios
+    //   .get("http://localhost:4000/user", {
+    //     withCredentials: true, // 쿠키가 전달이 된다.
+    //   })
+    //   .then(res => {
+    //     console.log(res.data);
+    //     this.setState({ userinfo: res.data });
+    //   });
   }
 
   render() {
@@ -27,7 +43,10 @@ class App extends React.Component {
         <Link to="/workspace">워크스페이스</Link>
         <Switch>
           <Route exact path="/" />
-          <Route path="/signin" render={() => <Signin />} />
+          <Route
+            path="/signin"
+            render={() => <Signin userLogin={this.userLogin} />}
+          />
           <Route path="/signup" render={() => <SignUp />} />
           <Route path="/workspace" />
         </Switch>
