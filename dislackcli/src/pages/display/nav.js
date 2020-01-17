8ea0657d8e556ab55b85c609c2fec2f57f534e22
LEEJAEHYUB <woguqdl20@gmail.com>
2020-01-18 02:37:18 +0900

    :lipstick: :sparkles: :art: :clown_face: Implement_Nav Search
      - 네비게이션에서 구현하여 렌더링하는 것 까지 완료
      - 검색이 어절별로 되는데 조합은 안됌...

--- dislackcli/src/pages/display/nav.js
+++ dislackcli/src/pages/display/nav.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { Row, Col, Icon, Input, Modal } from "antd";
+import { relative } from "path";
+import ModalRender from "./ModalRender";
 
 const { Search } = Input;
 
 class Nav extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { visible: false, searchMsg: null };
+    this.state = { visible: false, modalMsgs: null };
   }
 
-  showModal = e => {
-    console.log(e);
+  // 라이프 사이클로 컴포넌트디드업데이트
+
+  showModal = (e, val) => {
+    // console.log("모달스테이트", val);
     this.setState(() => ({
       visible: true,
       target: e,
     }));
 
-    console.log(this.state);
+    // console.log(this.state);
   };
 
   handleOk = e => {
@@ -32,26 +36,26 @@ class Nav extends React.Component {
   };
 
   // 현재 메세지들을 단어벌로 찾을 수 있게 검색하자
-  currentMsgs = item => {
+  currentMsgs = (item, cb) => {
     const searchArr = [];
     const curMsgs = this.props.msgs;
     curMsgs.map(val1 => {
-      console.log("메세지들? : ", val1);
+      // console.log("메세지들? : ", val1);
       const wordMsg = val1.message.split(" ");
       wordMsg.map(val2 => {
-        console.log("쪼갠메세지 : ", val2);
+        // console.log("쪼갠메세지 : ", val2);
         if (val2 === item) {
           searchArr.push(val1);
         }
       });
-      console.log("찾은단어들", searchArr);
+      // console.log("찾은단어들", searchArr);
+      cb(searchArr);
     });
   };
 
   searchSet(value) {
-    this.setState(() => {
-      this.setState({ searchMsg: value });
-    });
+    console.log("서치셋", value);
+    this.setState({ modalMsgs: value });
   }
 
   render() {
@@ -83,20 +87,37 @@ class Nav extends React.Component {
               />
             }
             onSearch={async item => {
-              await this.currentMsgs(item);
-              await this.showModal(item);
+              const ms = this.state.modalMsgs;
+              await this.currentMsgs(item, async res => {
+                await this.searchSet(res);
+                await this.showModal(item, ms);
+              });
             }}
             style={{ width: 200 }}
           />
           <Modal
-            title={this.state.target ? this.state.target : "Empty"}
+            title={
+              this.state.target ? (
+                <div>
+                  무엇을 원하는가...? <strong>{this.state.target}</strong>
+                </div>
+              ) : (
+                "Empty"
+              )
+            }
             visible={this.state.visible}
             onOk={this.handleOk}
             onCancel={this.handleCancel}
+            width={1400}
           >
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+            {this.state.modalMsgs ? (
+              this.state.modalMsgs.map(item => {
+                console.log("모달_매핑 ? ", item);
+                return <ModalRender renderMsg={item} key={Math.random()} />;
+              })
+            ) : (
+              <div />
+            )}
           </Modal>
 
           <Icon type="setting" />
