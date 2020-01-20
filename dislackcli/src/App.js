c271bb9d724964db54920c7569733600e0abebd8
Sooji Kim <yh0gogo@gmail.com>
2020-01-21 00:46:53 +0900

    :building_construction: Change App Routing
    - App.js에서 라우팅하는 부분을 2개 컴포넌트로 쪼갰습니다.

--- dislackcli/src/App.js
+++ dislackcli/src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
 import "./App.css";
-import { Redirect, Route, Switch, Link } from "react-router-dom";
-import { Row, Col, Button } from "antd";
 import axios from "axios";
-import Signin from "./pages/sign/SignIn";
-import SignUp from "./pages/sign/SignUp";
-import MainPage from "./pages/Main";
-import MyWorkSpace from "./pages/workspace/MyWorkSpace";
-import AllWorkSpace from "./pages/workspace/AllWorkSpace";
-import CreateWorkSpace from "./pages/workspace/createWorkSpace";
+import ToMain from "./route/ToMain";
+import ToWorkSpace from "./route/ToWorkSpace";
 import "antd/dist/antd.css";
-import Home from "./components/Home";
 
 class App extends React.Component {
   constructor(props) {
@@ -21,10 +14,24 @@ class App extends React.Component {
       currentWorkspace: null,
       workSpaceList: [],
     };
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
     this.getWorkSpace = this.getWorkSpace.bind(this);
     this.handleClickMyWS = this.handleClickMyWS.bind(this);
     this.updateWorkspace = this.updateWorkspace.bind(this);
     this.updateUserInfo = this.updateUserInfo.bind(this);
+  }
+
+  handleLogin() {
+    this.setState({
+      isLogin: true,
+      userInfo: JSON.parse(localStorage.getItem("userInfo")),
+    });
+  }
+
+  handleLogout() {
+    this.setState({ isLogin: false, userInfo: null });
+    alert("로그아웃 되었습니다!");
   }
 
   async getWorkSpace() {
@@ -106,164 +113,42 @@ class App extends React.Component {
 
   render() {
     const { isLogin, currentWorkspace, userInfo, workSpaceList } = this.state;
-    const { handleClickMyWS, updateWorkspace } = this;
+    const {
+      handleClickMyWS,
+      updateWorkspace,
+      handleLogin,
+      handleLogout,
+      getWorkSpace,
+      updateUserInfo,
+    } = this;
     // console.log("app.js state의 현재 선택된 워크스페이스", currentWorkspace);
     return isLogin && currentWorkspace ? (
       <div>
-        <Redirect to={`/main/${currentWorkspace[0].code}`} />
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => {
-              if (isLogin) {
-                return <Redirect to="/workspace" />;
-              }
-              return <Home />;
-            }}
-          />
-          <Route
-            path="/signin"
-            render={() => (
-              <Signin
-                isLogin={this.state.isLogin}
-                handleLogin={() => {
-                  this.setState({
-                    isLogin: true,
-                    userInfo: JSON.parse(localStorage.getItem("userInfo")),
-                  });
-                }}
-                getWorkSpace={this.getWorkSpace}
-              />
-            )}
-          />
-          <Route
-            path="/signup"
-            render={() => <SignUp isLogin={this.state.isLogin} />}
-          />
-          <Route
-            path="/workspace"
-            render={() => (
-              <div>
-                <div className="appWs-header">
-                  <div className="appWs-header-1">
-                    <Link className="appWs-header-1-1" to="/">
-                      Crong
-                    </Link>
-                  </div>
-                  <div className="appWs-header-2">
-                    <Link to="/workspace">Find your workspace</Link>
-                    <Link to="/signin">Sign in</Link>
-                  </div>
-                </div>
-                <Row>
-                  <Col span={12}>
-                    <MyWorkSpace
-                      isLogin={isLogin}
-                      userInfo={userInfo}
-                      workSpaceList={workSpaceList}
-                      handleClickMyWS={handleClickMyWS}
-                    />
-                  </Col>
-                  <Col span={12}>
-                    <AllWorkSpace
-                      isLogin={isLogin}
-                      userInfo={userInfo}
-                      workSpaceList={workSpaceList}
-                      updateWorkspace={updateWorkspace}
-                    />
-                  </Col>
-                </Row>
-                <Row style={{ marginBottom: "20%" }}>
-                  <Col span={8} />
-                  <Col span={8}>
-                    <CreateWorkSpace getWorkSpace={this.getWorkSpace} />
-                  </Col>
-                  <Col span={8} />
-                </Row>
-              </div>
-            )}
-          />
-          <Route
-            path={currentWorkspace ? `/main/${currentWorkspace[0].code}` : "/"}
-            render={() => (
-              <MainPage
-                className="MainPage"
-                isLogin={isLogin}
-                userInfo={userInfo}
-                workSpaceList={workSpaceList}
-                currentWorkspace={currentWorkspace}
-              />
-            )}
-          />
-        </Switch>
+        <ToMain
+          isLogin={isLogin}
+          currentWorkspace={currentWorkspace}
+          userInfo={userInfo}
+          workSpaceList={workSpaceList}
+          handleClickMyWS={handleClickMyWS}
+          updateWorkspace={updateWorkspace}
+          handleLogin={handleLogin}
+          handleLogout={handleLogout}
+          getWorkSpace={getWorkSpace}
+        />
       </div>
     ) : (
       <div className="App">
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <Home
-              handleLogout={() => {
-                this.setState({ isLogin: false, userInfo: null });
-                alert("로그아웃 되었습니다!");
-              }}
-            />
-          )}
-        />
-
-        <Route
-          path="/signin"
-          render={() => (
-            <Signin
-              isLogin={this.state.isLogin}
-              getWorkSpace={this.getWorkSpace}
-              updateUserInfo={this.updateUserInfo}
-              handleLogin={() => {
-                this.setState({
-                  isLogin: true,
-                  userInfo: JSON.parse(localStorage.getItem("userInfo")),
-                });
-              }}
-            />
-          )}
-        />
-        <Route
-          path="/signup"
-          render={() => <SignUp isLogin={this.state.isLogin} />}
-        />
-        <Route
-          path="/workspace"
-          render={() => (
-            <div>
-              <Row>
-                <Col span={12}>
-                  <MyWorkSpace
-                    isLogin={isLogin}
-                    userInfo={userInfo}
-                    workSpaceList={workSpaceList}
-                    handleClickMyWS={handleClickMyWS}
-                  />
-                </Col>
-                <Col span={12}>
-                  <AllWorkSpace
-                    isLogin={isLogin}
-                    userInfo={userInfo}
-                    workSpaceList={workSpaceList}
-                    updateWorkspace={updateWorkspace}
-                  />
-                </Col>
-              </Row>
-              <Row style={{ marginBottom: "20%" }}>
-                <Col span={8} />
-                <Col span={8}>
-                  <CreateWorkSpace getWorkSpace={this.getWorkSpace} />
-                </Col>
-                <Col span={8} />
-              </Row>
-            </div>
-          )}
+        <ToWorkSpace
+          isLogin={isLogin}
+          currentWorkspace={currentWorkspace}
+          userInfo={userInfo}
+          workSpaceList={workSpaceList}
+          handleClickMyWS={handleClickMyWS}
+          updateWorkspace={updateWorkspace}
+          handleLogin={handleLogin}
+          handleLogout={handleLogout}
+          getWorkSpace={getWorkSpace}
+          updateUserInfo={updateUserInfo}
         />
       </div>
     );
