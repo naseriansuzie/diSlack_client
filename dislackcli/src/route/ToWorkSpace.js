4c07ecb62ca23f1530940ae01d9cf9aa13db7565
LEEJAEHYUB <woguqdl20@gmail.com>
2020-01-21 12:03:06 +0900

    :building_construction: WorkSpace_ReFactor
      - 워크스페이스 리팩터 및 워크스페이스 조인 버그 수정
      - workspace 컴포넌트 생성
      - ToMain -> MainPage  / ToWorkSpace -> WorkSpace -> MainPage

--- dislackcli/src/route/ToWorkSpace.js
+++ dislackcli/src/route/ToWorkSpace.js
@@ -1,14 +1,11 @@
-//여기는 class 형으로 워크스페이스리스트를 마운트 시 불러옴
+// 여기는 class 형으로 워크스페이스리스트를 마운트 시 불러옴
 import React from "react";
 import "../App.css";
-import { Route, Switch, Link, Redirect } from "react-router-dom";
-import { Row, Col } from "antd";
-import axios from "axios";
+import { Route } from "react-router-dom";
+
 import Signin from "../pages/sign/SignIn";
 import SignUp from "../pages/sign/SignUp";
-import MyWorkSpace from "../pages/workspace/MyWorkSpace";
-import AllWorkSpace from "../pages/workspace/AllWorkSpace";
-import CreateWorkSpace from "../pages/workspace/createWorkSpace";
+import WorkSpace from "../pages/workspace/WorkSpace";
 import "antd/dist/antd.css";
 import Home from "../components/Home";
 import "../components/Home.css";
@@ -16,21 +13,18 @@ import "../components/Home.css";
 class ToWorkSpace extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { workSpaceList: this.props.workSpaceList };
   }
 
   render() {
     const {
       isLogin,
       userInfo,
-      workSpaceList,
-      handleClickMyWS,
       updateWorkspace,
       handleLogin,
       handleLogout,
-      getWorkSpace,
+      updateCurrentWorkspace,
+      updateUserInfo,
     } = this.props;
-    console.log("렌더가 되나?");
     return (
       <div>
         <Route
@@ -45,54 +39,28 @@ class ToWorkSpace extends React.Component {
 
         <Route
           path="/signin"
-          render={() => {
-            console.log("로그인");
-            return (
-              <div>
-                <Signin
-                  isLogin={isLogin}
-                  getWorkSpace={getWorkSpace}
-                  updateUserInfo={(item, email) =>
-                    this.updateUserInfo(item, email)
-                  }
-                  handleLogin={() => {
-                    handleLogin();
-                  }}
-                />
-              </div>
-            );
-          }}
+          render={() => (
+            <div>
+              <Signin
+                isLogin={isLogin}
+                updateUserInfo={(item, email) => updateUserInfo(item, email)}
+                handleLogin={handleLogin}
+              />
+            </div>
+          )}
         />
         <Route path="/signup" render={() => <SignUp isLogin={isLogin} />} />
         <Route
           path="/workspace"
           render={() => (
             <div>
-              <Row>
-                <Col span={12}>
-                  <MyWorkSpace
-                    isLogin={isLogin}
-                    userInfo={userInfo}
-                    workSpaceList={workSpaceList}
-                    handleClickMyWS={handleClickMyWS}
-                  />
-                </Col>
-                <Col span={12}>
-                  <AllWorkSpace
-                    isLogin={isLogin}
-                    userInfo={userInfo}
-                    workSpaceList={workSpaceList}
-                    updateWorkspace={updateWorkspace}
-                  />
-                </Col>
-              </Row>
-              <Row style={{ marginBottom: "20%" }}>
-                <Col span={8} />
-                <Col span={8}>
-                  <CreateWorkSpace getWorkSpace={getWorkSpace} />
-                </Col>
-                <Col span={8} />
-              </Row>
+              <WorkSpace
+                handleLogout={handleLogout}
+                isLogin={isLogin}
+                userInfo={userInfo}
+                updateWorkspace={updateWorkspace}
+                updateCurrentWorkspace={updateCurrentWorkspace}
+              />
             </div>
           )}
         />
