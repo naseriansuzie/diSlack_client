1a1ce577735a4e49d9cbf8c77550fe00b66ce3f8
LEEJAEHYUB <woguqdl20@gmail.com>
2020-01-23 02:39:04 +0900

    :lipstick: MessageList_CSS

--- dislackcli/src/pages/display/MessageEntries.js
+++ dislackcli/src/pages/display/MessageEntries.js
@@ -24,6 +24,16 @@ export default function MessageEntries(props) {
     ) : (
       <div className="noReply" style={{ height: 0 }} />
     );
+
+  const imgs = [
+    "https://ca.slack-edge.com/TEYSE8X7A-UGZKRTKS7-g1211904810f-512",
+    "https://ca.slack-edge.com/TEYSE8X7A-UGTATM4SZ-gb1c54dce61e-512",
+    "https://ca.slack-edge.com/TEYSE8X7A-UGT1WBY5T-g29afd645804-512",
+    "https://ca.slack-edge.com/TEYSE8X7A-UGT2ASV3P-g125e42e1fcb-512",
+    "https://ca.slack-edge.com/TEYSE8X7A-UF0L7NKM5-g387a4cd39e1-512",
+  ];
+  const num = Math.floor(Math.random() * 4) + 1;
+
   return (
     <div id={id} style={{ padding: 0 }}>
       <Comment
@@ -33,7 +43,7 @@ export default function MessageEntries(props) {
             <a onClick={handleClickProfile.bind(null, user.id)}>{user.name}</a>
           </strong>
         }
-        avatar={<Avatar shape="square" size="large" icon="user" />}
+        avatar={<Avatar shape="square" src={`${imgs[num]}`} />}
         content={
           <div>
             {message || reply}
